7d9be37f2b3df973de4681d5a631d5934ecaa70b
Kenard Ellis <kingkenard5@gmail.com>
2020-12-13 19:26:27 -0500

    first commmit

---
+++ main.js
@@ -0,0 +1,8 @@
+const ul = document.querySelector(".nav-links");
+const toggler = document.querySelector(".toggler");
+ul.addEventListener('click',hidden);
+    function hidden(ev){
+        toggler.checked = false;
+        }
+
+console.log('we are here');
